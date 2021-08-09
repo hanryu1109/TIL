bbe45c55cd832feab3c1f6b90a25722351a0d54d
hanryu1109 <hanryu1109@gmail.com>
2021-08-09 23:48:50 +0900

    create 12~14 problems solutions

---
+++ programmers/13_level1_13.js
@@ -0,0 +1,22 @@
+//문제 : 문자열을 정수로 바꾸기
+//입출력 예
+//n = 12345, return = [5, 4, 3, 2, 1]
+
+//best
+function strToInt(str){
+    return str/1
+}
+
+//best
+function strToInt(str){
+    return  +str;
+}
+
+//my trial
+function solution(s) {
+    return Number(s);
+}
+
+// Today I Learned
+// 자바스크립트에서는 문자열을 1로 나누면 데이터 타입이 숫자로 바뀐다
+// 자바스크립트에서는 문자열 앞에 + 기호를 붙이면 데이터 타입이 숫자로 바뀐다.
---
+++ programmers/14_level1_14.js
@@ -0,0 +1,14 @@
+//문제 : 서울에서 김서방 찾기
+//입출력 예
+//seoul = ["Jane", "Kim"], return = "김서방은 1에 있다"
+
+//best return  +str;
+}
+
+//my trial
+function solution(seoul) {
+    return `김서방은 ${seoul.indexOf("Kim")}에 있다`;
+}
+
+// Today I Learned
+// 
